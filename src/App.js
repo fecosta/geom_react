fa46439840f0c42013df6241f19325d9a1d7aefb
Felipe Costa <costa.felipe@gmail.com>
2024-05-20 22:34:30 -0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
+import{ BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './Home';
-import{ BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Maps from './Maps';
 import Table from './Table';
 import Charts from './Charts';
--- src/Maps.js
+++ src/Maps.js
@@ -6,4 +6,4 @@ const Maps = () => {
     );
 }
  
-export default Maps ;
+export default Maps;
